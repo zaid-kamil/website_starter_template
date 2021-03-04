c49d6c6bf84dfef3bcc627f20ab7ff34749fe895
Zaid <xaidmetamorphos@gmail.com>
2021-03-04 15:19:33 +0530

    update_content

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,5 @@
+from projects.models import Project
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Project)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    """Model definition for Project."""
+    title = models.CharField(max_length=100,default='project')
+    description = models.TextField(default='No Description given')
+    technology = models.CharField(max_length=50, default='Django')
+    image = models.FilePathField(path='/img', default='/img/noimage.png')
+
+    class Meta:
+        """Meta definition for Project."""
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        """Unicode representation of Project."""
+        return self.title
