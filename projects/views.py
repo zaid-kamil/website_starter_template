5e1062ef87f610105c8a051ed3e1bdd815721983
Zaid <xaidmetamorphos@gmail.com>
2021-03-08 14:31:02 +0530

    ab aage

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Project
 
 # Create your views here.
+
 def project_list_view(request):
     projectlist = Project.objects.all()
     return render(request,'index.html',context={'projects':projectlist})
