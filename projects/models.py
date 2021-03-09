b61a82fbb800f089e59ea6e4e9da66ce777e8472
Zaid <xaidmetamorphos@gmail.com>
2021-03-09 15:29:33 +0530

    database done

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100,default='project')
     description = models.TextField(default='No Description given')
     technology = models.CharField(max_length=50, default='Django')
-    # image = models.FilePathField(path='/img', default='/img/noimage.png')
+    img = models.ImageField(upload_to='projects/', default='projects/noimg.jpg')
 
     class Meta:
         """Meta definition for Project."""
--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,4 @@ from .views import project_list_view, project_detail_view
 urlpatterns = [
     path('',project_list_view, name='project'),
     path('<int:pk>/',project_detail_view, name='project_detail')
-    +
 ]
